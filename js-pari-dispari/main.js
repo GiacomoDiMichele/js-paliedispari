492516a4074bc01db89d76e9f56f5d7585815aaf
Giacomo <giacomo.1936@gmail.com>
2020-10-21 17:48:10 +0200

    initial commit

---
+++ js-pari-dispari/main.js
@@ -0,0 +1,16 @@
+var numero_utente = parseInt(prompt('inserisci un numero'));
+var pariodispari = prompt('scegli tra pari o dispari');
+var numero_computer = Math.floor(Math.random() * 5);
+var pari = numero_utente % 2 == 0;
+var dispari = numero_utente % 2 == 1;
+console.log(numero_utente);
+console.log(pariodispari);
+console.log(numero_computer);
+risultato_finale = numero_utente + numero_computer
+console.log(somma);
+
+function utente_uno(numero_utente, pariodispari) {
+    if (risultato_finale == pari) {
+
+    }
+}
